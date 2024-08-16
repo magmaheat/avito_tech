2d5729ce4fe5253260a682f7187defaa0664ff0b
Quinton Stephen <quintons@ge-e5.kzn.21-school.ru>
2024-08-16 21:24:02 +0300

    created templates test

--- internal/http_server/handlers/flat/flat_test.go
+++ internal/http_server/handlers/flat/flat_test.go
@@ -1,1 +1,29 @@
 package flat
+
+import (
+	"log/slog"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	type args struct {
+		log     *slog.Logger
+		storage FlatStorage
+	}
+	tests := []struct {
+		name string
+		args args
+		want http.HandlerFunc
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Create(tt.args.log, tt.args.storage); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Create() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
--- internal/http_server/handlers/house/house_test.go
+++ internal/http_server/handlers/house/house_test.go
@@ -1,6 +1,53 @@
 package house
 
-import "testing"
+import (
+	"log/slog"
+	"net/http"
+	"reflect"
+	"testing"
+)
 
 func TestHappyCreate(t *testing.T) {
+}
+
+func TestCreate(t *testing.T) {
+	type args struct {
+		log     *slog.Logger
+		storage HouseStorage
+	}
+	tests := []struct {
+		name string
+		args args
+		want http.HandlerFunc
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Create(tt.args.log, tt.args.storage); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Create() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFlats(t *testing.T) {
+	type args struct {
+		log     *slog.Logger
+		storage HouseStorage
+	}
+	tests := []struct {
+		name string
+		args args
+		want http.HandlerFunc
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Flats(tt.args.log, tt.args.storage); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Flats() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
